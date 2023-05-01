4dbdb1d91b57fe037fbb95825c24b9cb307308a0
knovvX <1355426436@qq.com>
2023-05-01 15:30:38 +0800

    feat：之前的try

--- vast_2021_mc2/src/components/js/TLines.js
+++ vast_2021_mc2/src/components/js/TLines.js
@@ -1,73 +1,45 @@
-import * as THREE from 'three';
+import {
+    LineBasicMaterial,
+    BufferGeometry,
+    Vector3,
+    Line,
+    // Color,
+    // MeshLambertMaterial,
+    // DoubleSide,
+    // Mesh
+} from "three"
+// import {FontLoader} from "three/examples/jsm/loaders/FontLoader";
 
-// import Stats from 'three/addons/libs/stats.module.js';
-// import { GPUStatsPanel } from 'three/addons/utils/GPUStatsPanel.js';
-
-// import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
-// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-// import { Line2 } from 'three/examples/jsm/lines/Line2.js';
-// import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js';
-// import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry.js';
-// import * as GeometryUtils from 'three/addons/utils/GeometryUtils.js';
-// import * as d3 from "d3";
-import trackJson from './tracks.json';
-let line,matLine;
-// let insetWidth, insetHeight;
+// import {Line2} from "three/examples/jsm/lines/Line2.js"
+import tkJson from './tracks.json'
+import * as d3 from "d3";
 export const allTrackLines = []
-trackJson.forEach(element => {
+
+var material = new LineBasicMaterial({ color: 0xFF0000 });
+/*
+1.2.5版本就不再支持这个api了，如果还需要就用他的子类BufferGeometry。
+var geometry = new THREE.Geometry();
+*/
+
+let xScale = d3.scaleLinear().domain([20, 30]).range([-1035, 1095]);//地图太小了 比例尺放大
+let yScale = d3.scaleLinear().domain([30, 40]).range([-1290, 840]);
+let cnt=0;
+tkJson.forEach(element => {
+    cnt+=1;
+    const geometry = new BufferGeometry();
     const pointsArray = [];
-    const coordinates = element.coordinates;
-    // let xScale = d3.scaleLinear().domain([20,30]).range([-1035,1095]);//地图太小了 比例尺放大
-    // let yScale = d3.scaleLinear().domain([30,40]).range([-1290,840]);
     if (element.date == "2014-01-06" && element.carID == "1") {
+        const coordinates = element.coordinates;
         for (let i = 0; i < coordinates.length; i++) {
-            pointsArray.push(new THREE.Vector3((coordinates[i][0]/6),5,(coordinates[i][1]/6)));
-
+            pointsArray.push(new Vector3(xScale(coordinates[i][1]),0.5, yScale(coordinates[i][0])));
         }
-        console.log(pointsArray.length)
+
+        geometry.setFromPoints(pointsArray);
+        // console.log(pointsArray[0])
+
+        var line = new Line(geometry, material); 
+        allTrackLines.push(line);
+        // console.log(line)
     }
-    // const colors = new THREE.Color();
-    const geometry = new THREE.BufferGeometry();
-    geometry.setFromPoints(pointsArray);
-    // geometry.setColors(colors);
-    // console.log("here");
-    // matLine = new LineMaterial({
-
-    //     color: 0xffffff,
-    //     linewidth: 5, // in world units with size attenuation, pixels otherwise
-    //     vertexColors: true,
-
-    //     //resolution:  // to be set by renderer, eventually
-    //     dashed: false,
-    //     alphaToCoverage: true,
-
-    // });
-    matLine = new THREE.LineBasicMaterial({color: 0x0000ff,linewidth:25});
-    
-    // line = new Line2(geometry, matLine);
-    // line.computeLineDistances();
-    // line.scale.set(1, 1, 1);
-    // allTrackLines.push(line);
-    // insetWidth = window.innerHeight / 4; // square
-    // insetHeight = window.innerHeight / 4;
-    // matLine.resolution.set( insetWidth, insetHeight ); // resolution of the inset viewport
-    line = new THREE.Line( geometry, matLine );
-    allTrackLines.push(line);
-    // console.log(allTrackLines)
-});
-
-// var loader = new FontLoader();
-
-// loader.load( 'Microsoft YaHei_Regular.json', function ( font ) {
-
-//     let textGeometry = new TextGeometry('???????????????', {
-//         font: font,
-//         size: 200,
-//         height: 0.1,
-//         curveSegments: 12,
-//     });
-//     let mesh = new Mesh(textGeometry, font2dMaterial);
-//     mesh.position.set(-8, 0, 0);
-//     allBaseObject.push(mesh)
-//     console.log(allBaseObject)
-// });
+})
+console.log(cnt)
