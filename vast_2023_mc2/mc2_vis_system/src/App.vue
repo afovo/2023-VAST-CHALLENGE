25871e394aa734ae5317a54dec6b48b32f2cb797
afovo <12011437@mail.sustech.edu.cn>
2023-05-14 00:36:45 +0800

    add: mc2_vis_system/d3HBundleGraph

---
+++ vast_2023_mc2/mc2_vis_system/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <d3-h-bundle-graph></d3-h-bundle-graph>
+</template>
+<style scoped>
+</style>
+<script setup>
+import d3HBundleGraph from "./components/d3HBundleGraph.vue"
+import D3HBundleGraph from "./components/d3HBundleGraph.vue";
+</script>
