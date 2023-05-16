882266632c31f86fe81f3a4319b3b83bb93a554d
afovo <12011437@mail.sustech.edu.cn>
2023-05-17 01:39:45 +0800

    bug: try on mc2 data, but rendering error

--- vast_2023_mc2/mc2_vis_system/src/App.vue
+++ vast_2023_mc2/mc2_vis_system/src/App.vue
@@ -1,9 +1,10 @@
 <template>
+<!--  <graph-change-on-date></graph-change-on-date>-->
   <d3-time-force></d3-time-force>
 </template>
 <style scoped>
 </style>
 <script setup>
-import D3HBundleGraph from "./components/d3HBundleGraph.vue";
 import D3TimeForce from "./components/d3TimeForce.vue"
+// import GraphChangeOnDate from "./components/graphChangeOnDate.vue"
 </script>
